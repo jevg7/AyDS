ad63695c168b6849527ac36608a8dd155b68f6e6
Jairo <jvegag@uamv.edu.ni>
2024-11-12 21:21:57 -0600

    g

--- zasApp/Pantallaprinci.Designer.cs
+++ zasApp/Pantallaprinci.Designer.cs
@@ -35,8 +35,8 @@
             this.btnherramientas = new System.Windows.Forms.Button();
             this.btnEquipamiento = new System.Windows.Forms.Button();
             this.btnreactivos = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -54,98 +54,107 @@
             this.panel1.Location = new System.Drawing.Point(-1, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(218, 849);
+            this.panel1.Size = new System.Drawing.Size(164, 690);
             this.panel1.TabIndex = 0;
             // 
             // btnCerrarSesion
             // 
-            this.btnCerrarSesion.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrarSesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCerrarSesion.ForeColor = System.Drawing.Color.Red;
-            this.btnCerrarSesion.Location = new System.Drawing.Point(13, 796);
+            this.btnCerrarSesion.Location = new System.Drawing.Point(10, 647);
+            this.btnCerrarSesion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCerrarSesion.Name = "btnCerrarSesion";
-            this.btnCerrarSesion.Size = new System.Drawing.Size(188, 26);
+            this.btnCerrarSesion.Size = new System.Drawing.Size(141, 21);
             this.btnCerrarSesion.TabIndex = 6;
             this.btnCerrarSesion.Text = "Cerrar Sesion";
             this.btnCerrarSesion.UseVisualStyleBackColor = true;
             // 
             // btnUsuarios
             // 
-            this.btnUsuarios.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUsuarios.Location = new System.Drawing.Point(30, 622);
+            this.btnUsuarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUsuarios.Location = new System.Drawing.Point(15, 505);
+            this.btnUsuarios.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnUsuarios.Name = "btnUsuarios";
-            this.btnUsuarios.Size = new System.Drawing.Size(163, 40);
+            this.btnUsuarios.Size = new System.Drawing.Size(129, 32);
             this.btnUsuarios.TabIndex = 5;
             this.btnUsuarios.Text = "Usuarios";
             this.btnUsuarios.UseVisualStyleBackColor = true;
             // 
             // btnOrdenes
             // 
-            this.btnOrdenes.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOrdenes.Location = new System.Drawing.Point(30, 520);
+            this.btnOrdenes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrdenes.Location = new System.Drawing.Point(15, 422);
+            this.btnOrdenes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnOrdenes.Name = "btnOrdenes";
-            this.btnOrdenes.Size = new System.Drawing.Size(163, 45);
+            this.btnOrdenes.Size = new System.Drawing.Size(129, 37);
             this.btnOrdenes.TabIndex = 4;
             this.btnOrdenes.Text = "Ordenes";
             this.btnOrdenes.UseVisualStyleBackColor = true;
             // 
             // btnherramientas
             // 
-            this.btnherramientas.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnherramientas.Location = new System.Drawing.Point(30, 424);
+            this.btnherramientas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnherramientas.Location = new System.Drawing.Point(15, 344);
+            this.btnherramientas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnherramientas.Name = "btnherramientas";
-            this.btnherramientas.Size = new System.Drawing.Size(163, 47);
+            this.btnherramientas.Size = new System.Drawing.Size(129, 38);
             this.btnherramientas.TabIndex = 3;
             this.btnherramientas.Text = "Herramientas";
             this.btnherramientas.UseVisualStyleBackColor = true;
             // 
             // btnEquipamiento
             // 
-            this.btnEquipamiento.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEquipamiento.Location = new System.Drawing.Point(30, 322);
+            this.btnEquipamiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEquipamiento.Location = new System.Drawing.Point(15, 263);
+            this.btnEquipamiento.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnEquipamiento.Name = "btnEquipamiento";
-            this.btnEquipamiento.Size = new System.Drawing.Size(163, 46);
+            this.btnEquipamiento.Size = new System.Drawing.Size(129, 37);
             this.btnEquipamiento.TabIndex = 2;
             this.btnEquipamiento.Text = "Equipamiento";
             this.btnEquipamiento.UseVisualStyleBackColor = true;
             // 
             // btnreactivos
             // 
-            this.btnreactivos.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnreactivos.Location = new System.Drawing.Point(30, 227);
+            this.btnreactivos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnreactivos.Location = new System.Drawing.Point(15, 184);
+            this.btnreactivos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnreactivos.Name = "btnreactivos";
-            this.btnreactivos.Size = new System.Drawing.Size(163, 42);
+            this.btnreactivos.Size = new System.Drawing.Size(129, 34);
             this.btnreactivos.TabIndex = 1;
             this.btnreactivos.Text = "Reactivos";
             this.btnreactivos.UseVisualStyleBackColor = true;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("MoeumT R", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(340, 407);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(876, 178);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "¡Zas! Medical\r\nSistema Administrativo\r\n";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Zas_Sistema_Administrativo_y_Inventario.Properties.Resources.Zas_log;
-            this.pictureBox1.Location = new System.Drawing.Point(47, 39);
+            this.pictureBox1.Location = new System.Drawing.Point(35, 32);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(130, 108);
+            this.pictureBox1.Size = new System.Drawing.Size(98, 88);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(255, 331);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(706, 146);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "¡Zas! Medical\r\nSistema Administrativo\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // Pantallaprinci
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1578, 843);
+            this.ClientSize = new System.Drawing.Size(1184, 685);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Pantallaprinci";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
